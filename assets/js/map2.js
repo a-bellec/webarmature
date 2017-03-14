3ed34242db095d586adc49a58700ec3b6deea604
Jean-Sébastien Perreault <jean-sebperro@outlook.com>
2017-03-14 13:20:07 +0100

    Collapse other sidebar when doing action on one of the sidebar

--- assets/js/map2.js
+++ assets/js/map2.js
@@ -1,5 +1,7 @@
+//Define the center of the map
 var center = [45.7604276, 4.8335709];
 
+//Create left map
 var map1 = L.map('map1', {
     center: center,
     zoom: 16,
@@ -7,6 +9,7 @@ var map1 = L.map('map1', {
     minZoom: 13
 });
 
+//Create right map
 var map2 = L.map('map2', {
     center: center,
     zoom: 16,
@@ -14,32 +17,40 @@ var map2 = L.map('map2', {
     minZoom: 13
 });
 
+//SetZoomePosition
 map1.zoomControl.setPosition('topright');
 map2.zoomControl.setPosition('topright');
 
+//Sync zoom of the 2 maps
 map1.sync(map2, {syncCursor: true});
 map2.sync(map1, {syncCursor: true});
 
+//Set default map
 var LOSM = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }
 ).addTo(map1);
 
+//Set default map
 var ROSM = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }
 ).addTo(map2);
 
+//Upon change of map selection
 $("input[type=radio]").on('switchChange.bootstrapSwitch', function (e, s) {
 
+    //Create the new layer
     var layerName = this.id;
     var layer = L.tileLayer.wms('http://a.map.webarmature.fr/geoserver/wms/', {
         layers: layerName,
         transparent: true
     });
 
+    //If checking left group
     if (this.checked && this.name == "GroupedSwitchesL") {
         removeAllMap1Layers();
+        //Every map has the same structure except the default one. Make sure you don't have the default one selected
         if (layerName != "LOSM") {
             map1.addLayer(layer);
         }
@@ -50,6 +61,7 @@ $("input[type=radio]").on('switchChange.bootstrapSwitch', function (e, s) {
             ).addTo(map1);
         }
     }
+    //If checking right group
     else if(this.checked && this.name == "GroupedSwitchesR"){
         removeAllMap2Layers();
         if (layerName != "ROSM") {
@@ -79,3 +91,18 @@ function removeAllMap2Layers() {
 }
 
 L.popup();
+
+//Collapse and put inactive the selected element of the other sidebar on click
+$("#sidebar_right").click(function(){
+   $("#sidebar_left").addClass("collapsed");
+   $("#sidebar_left-elements").find("li").each(function(){
+        $(this).removeClass("active");
+   });
+});
+
+$("#sidebar_left").click(function(){
+    $("#sidebar_right").addClass("collapsed");
+    $("#sidebar_right-elements").find("li").each(function(){
+        $(this).removeClass("active");
+    });
+});
