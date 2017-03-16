393a9cf565509c5e738f457df6be82f6bb79fcba
Jean-Sébastien Perreault <jean-sebperro@outlook.com>
2017-03-16 10:51:36 +0100

    Create a file to reference map layer name and attribution

---
+++ assets/js/mapReferences.js
@@ -0,0 +1,27 @@
+var mapsOrthophotographies = [
+    ["data_all_1954", "Année : 1954 - &copy; <a href='http://www.geoportail.gouv.fr/'>IGN - Photos aériennes 1954</a>"],
+    ["data_all_1966", "Année : 1966 - &copy; <a href='http://www.geoportail.gouv.fr/'>IGN - Photos aériennes 1966</a>"],
+    ["data_all_1978", "Année : 1978 - &copy; <a href='http://www.geoportail.gouv.fr/'>IGN - Photos aériennes 1978</a>"],
+    ["data_all_1986", "Année : 1986 - &copy; <a href='http://www.geoportail.gouv.fr/'>IGN - Photos aériennes 1986</a>"],
+    ["data_all_1993", "Année : 1993 - &copy; <a href='http://www.grandlyon.com/'> Grand Lyon orthophotographie 1993</a>"],
+    ["data_all_1997", "Année : 1997 - &copy; <a href='http://www.grandlyon.com/'> Grand Lyon orthophotographie 1997</a>"],
+    ["data_all_2003", "Année : 2003 - &copy; <a href='http://www.grandlyon.com/'> Grand Lyon orthophotographie 2003</a>"],
+    ["data_all_2007", "Année : 2007 - &copy; <a href='http://www.geoportail.gouv.fr/'> Photos aériennes 2007</a>"]
+];
+
+var mapsScan25 = [
+    ["scan25_all_1950", "Année : 1950 - &copy; <a href='http://ign.fr/'> Scan 25 1950</a>"],
+    ["scan25_all_1960", "Année : 1960 - &copy; <a href='http://ign.fr/'> Scan 25 1960</a>"],
+    ["scan25_all_1970", "Année : 1970 - &copy; <a href='http://ign.fr/'> Scan 25 1970</a>"],
+    ["scan25_all_1985", "Année : 1985 - &copy; <a href='http://ign.fr/'> Scan 25 1985</a>"],
+    ["scan25_lyon_1998", "Année : 1998 - &copy; <a href='http://ign.fr/'> Scan 25 1998</a>"],
+    ["scan25_all_2003", "Année : 2003 - &copy; <a href='http://ign.fr/'> Scan 25 2003</a>"],
+    ["scan25_all_2015", "Année : 2015 - &copy; <a href='http://ign.fr/'> Scan 25 2015</a>"]
+];
+
+var mapsScan50 = [
+    ["scan50_all_1950", "Année : 1950 - &copy; <a href='http://ign.fr/'> Scan 50 1950</a>"],
+    ["scan50_all_1960_clip", "Année : 1960 - &copy; <a href='http://ign.fr/'> Scan 50 1960</a>"],
+    ["scan50_all_1970_clip", "Année : 1970 - &copy; <a href='http://ign.fr/'> Scan 50 1970</a>"],
+    ["scan50_all_1980_clip", "Année : 1980 - &copy; <a href='http://ign.fr/'> Scan 50 1980</a>"]
+];
