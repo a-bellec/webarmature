784d100b9c9419c7d057a39ad0a71d943565df09
Jean-Sébastien Perreault <jean-sebperro@outlook.com>
2017-03-16 10:39:58 +0100

    Attribution in map2

--- assets/js/map2.js
+++ assets/js/map2.js
@@ -42,9 +42,11 @@ $("input[type=radio]").on('switchChange.bootstrapSwitch', function (e, s) {
 
     //Create the new layer
     var layerName = this.value;
+    var attribution = this.alt;
     var layer = L.tileLayer.wms('http://a.map.webarmature.fr/geoserver/wms/', {
         layers: layerName,
-        transparent: true
+        transparent: true,
+        attribution: attribution
     });
 
     if(this.checked){
