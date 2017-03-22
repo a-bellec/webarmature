24adf5b79dfaffec4c64cf9478faee3f4e4d24bc
Jean-Sébastien Perreault <jean-sebperro@outlook.com>
2017-03-22 18:24:32 +0100

    Creation of pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+
+  apps : [
+
+    // First application
+    {
+      name      : "webarmature",
+      script    : "dist/server/index.js",
+      env_production : {
+        NODE_ENV: "production"
+      }
+    }
+  ],
+
+  deploy : {
+    production : {
+      user : "node",
+      host : "webarmature.fr",
+      ref  : "origin/master",
+      repo : "git@github.com:abellec29/webarmature.git",
+      path : "/var/www/production",
+      "post-deploy" : "npm install && gulp build && pm2 startOrRestart ecosystem.config.js --env production"
+    },
+    dev : {
+      user : "node",
+      host : "webarmature.fr",
+      ref  : "origin/master",
+      repo : "git@github.com:abellec29/webarmature.git",
+      path : "/var/www/development",
+      "post-deploy" : "npm install && gulp build && pm2 startOrRestart ecosystem.config.js --env dev",
+      env  : {
+        NODE_ENV: "dev"
+      }
+    }
+  }
+};
