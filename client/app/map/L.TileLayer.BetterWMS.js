06e5a58e7377120aa27619cd4b717f5d1b9f510d
Jean-Sébastien Perreault <jean-sebperro@outlook.com>
2017-04-07 17:33:22 +0200

    ignore town layer

--- client/app/map/L.TileLayer.BetterWMS.js
+++ client/app/map/L.TileLayer.BetterWMS.js
@@ -38,6 +38,10 @@ L.TileLayer.BetterWMS = L.TileLayer.WMS.extend({
   },
 
   getMapInfoUrl(){
+    if(this.wmsParams.layers == "towns_border-d2015"){
+      return "";
+    }
+
     let mapBounds = this._map.getBounds();
 
     proj4.defs('EPSG:2154', "+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs ");
