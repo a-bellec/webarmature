90e83657a8e4506a29275aef3c3e51a58cccf44f
Jean-Sébastien Perreault <jean-sebperro@outlook.com>
2017-04-18 15:29:25 +0200

    Add legend and use svg directly

--- client/app/map/mapDirectives/statAreaDirective/statArea.directive.js
+++ client/app/map/mapDirectives/statAreaDirective/statArea.directive.js
@@ -17,22 +17,20 @@ export default angular.module('webarmatureApp.statArea', [])
     $scope.printChart = function(dataset){
 
       //remove svg to draw new chart
-      d3.select("#" + $scope.chartId + "SVG").remove();
-
+      d3.select("#" + $scope.chartId + " g").remove();
+      
       var height = $("stat-area").height();
-      var width = height;
+      var width = $("stat-area").width();
       var radius = Math.min(width, height) / 2;
 
       var color = d3.scaleOrdinal()
         .range(["#006a01", "#00b515" , "#e2d920", "#f85402", "#a40005"]);
 
       var svg = d3.select('#' + $scope.chartId)
-        .append('svg')
-        .attr('id', $scope.chartId + "SVG")
         .attr('width', width)
         .attr('height', height)
         .append('g')
-        .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
+        .attr('transform', 'translate(' + width/2 +  ',' + (height / 2) + ')');
 
       var arc = d3.arc()
         .innerRadius(0)
@@ -50,6 +48,42 @@ export default angular.module('webarmatureApp.statArea', [])
         .attr('fill', function(d, i) {
           return color(d.data.label);
         });
+
+      var legendRectSize = 18;
+      var legendSpacing = 4;
+
+      var legend = svg.selectAll('.legend')
+        .data(color.domain())
+        .enter()
+        .append('g')
+        .attr('class', 'legend')
+        .attr('transform', function(d, i){
+          var height = legendRectSize + legendSpacing;
+          var offset =  height * color.domain().length / 2;
+          var horz = $("stat-area").height()/2 + 10;
+          var vert = i * height - offset;
+          return 'translate(' + horz + ',' + vert + ')';
+        });
+
+      legend.append('rect')
+        .attr('width', legendRectSize)
+        .attr('height', legendRectSize)
+        .style('fill', color)
+        .style('stroke', color);
+
+      legend.append('text')
+        .data(dataset)
+        .attr('x', legendRectSize + legendSpacing)
+        .attr('y', legendRectSize - legendSpacing)
+        .text(function(d, i) {
+          return i*20 + "-" + (i*20 + 20) + "%: " + d.count + "%";
+        });
+
+      let title = svg.append("text")
+        .attr("x", width/2)
+        .attr("y", 0 - (height/2))
+        .style("font-size", "14px")
+        .text("Pourcentage imperméable: " + $scope.dataAverage + "%");
     };
 
     $scope.$watch('dataset.impermeable', function(){
